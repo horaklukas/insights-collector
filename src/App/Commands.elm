377bccc8520f15b3d0d2ea0625cc435ece1bbf9d
Lukas Horak <horaklukas@centrum.cz>
2016-11-13 20:13:49 +0100

    Start preparing for production, at this time, build is broken

--- src/App/Commands.elm
+++ src/App/Commands.elm
@@ -3,16 +3,23 @@ module App.Commands exposing (fetchWebpages)
 import Http
 import Json.Decode as Json
 import Task
+import Navigation exposing (Location)
 
 import App.Messages exposing (AppMsg (..))
 import WebReport.Models exposing (WebUrl)
 
-dbUrl: String
-dbUrl = "http://localhost:4000"
+urlParser : Navigation.Location -> String
+urlParser location =
+    if location.port_ == "3000" then "http://localhost:4000" else "."
+
+parser : Navigation.Parser (String)
+parser =
+    Navigation.makeParser urlParser
 
 fetchWebpages: Cmd AppMsg
 fetchWebpages =
   let
+    dbUrl = parser 
     webpagesUrl = dbUrl ++ "/webpages"
   in
     Task.perform FetchWebpagesFail FetchReports (Http.get decodeCollection webpagesUrl)
